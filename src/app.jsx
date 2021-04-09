5ee37d0f785123f54d1f7f63eee5b16688a0acb9
byungju0624 <byungju0624@gmail.com>
2021-04-09 23:57:43 +0900

    test

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./app.css";
-import Button from "./components/button/button";
+
 import Repogitory from "./components/repogitory/repogitory";
 function App() {
   return (
